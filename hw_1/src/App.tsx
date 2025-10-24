71bd7d0e853f74d64a2313f03887badf7208d2d3
nick <11andriianovnikita@gmail.com>
2025-10-24 23:01:30 +0300

    есть поиск

--- hw_1/src/App.tsx
+++ hw_1/src/App.tsx
@@ -1,10 +1,33 @@
-import FilmCard from "./components/filmCard/FilmCard.tsx";
 import {getMockFilms} from "./model/film.ts";
+import FilmCardList from "./components/filmCardList/FilmCardList.tsx";
+import {useState, useRef} from "react";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const handleFind = () => {
+    if (inputRef.current) {
+      setSearch(inputRef.current.value);
+    }
+  }
+
+  const onKeyDownInInput = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleFind();
+    }
+  }
+
   return (
     <>
-      <FilmCard film={getMockFilms()[0]}/>
+      <div>
+        <input ref={inputRef} onKeyDown={onKeyDownInInput}/>
+        <button onClick={handleFind}>
+          Найти
+        </button>
+      </div>
+
+      <FilmCardList films={getMockFilms()} search={search} />
     </>
   )
 }
---
+++ hw_1/src/components/filmCardList/FilmCardList.tsx
@@ -0,0 +1,25 @@
+import FilmCard from "../filmCard/FilmCard.tsx";
+import type {Film} from "../../model/film.ts";
+import {useMemo} from "react";
+
+interface FilmCardListProps {
+  films: Film[];
+  search: string;
+}
+
+function FilmCardList({films, search}: FilmCardListProps) {
+  const filmsList = useMemo(() => {
+    if (search.length < 1) return films;
+    return films.filter(film => film.title.toLowerCase().includes(search.toLowerCase()));
+  }, [films, search]);
+
+  return (
+    <div>
+      {filmsList.length > 0 && filmsList.map((film) => (
+        <FilmCard key={film.id} film={film} />
+      ))}
+    </div>
+  );
+}
+
+export default FilmCardList;
